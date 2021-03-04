ac2700141a6db5e13eed8afd21e01226d21e9618
kavinsurya <kavinsurya002@gmail.com>
2021-03-04 11:16:49 +0530

    Home page Image added

---
+++ src/generalComponents/HomePage/HomePageImage/HomePageImage.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./HomePageImage";
+import Carousel from "react-bootstrap/Carousel";
+
+export function HomePageImage  ()  {
+  const [data, setData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    const userList = async () => {
+      let response = await fetch("http://localhost:5000/movie/movies");
+      let user = await response.json();
+      setData(user);
+    };
+    userList();
+  }, []);
+  return (
+    <div>
+      <Carousel className="homepage-carousel">
+        {data.map((movie, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              srcSet={movie.poster}
+              alt=" "
+              height="675px"
+              width="100%"
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+
---
+++ src/generalComponents/HomePage/HomePageImage/HomePageImg.jsx
@@ -0,0 +1,22 @@
+import { React } from "react";
+import "./HomePageImage.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+export function HomePageImg() {
+
+
+    return (
+        <div className="text-center homepageImage col-12">
+            <img style={{}}
+                srcSet="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-288/web-revival_discoverywidget-collection-202010130410.jpg"
+                alt=""
+                className="rounded"
+                width="100%"
+            />
+        </div>
+
+    );
+};
+
